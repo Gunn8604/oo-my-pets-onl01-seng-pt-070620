caba68818e5b6a5d1418fbeb52b23d3abf1b2919
Billy Gunn, Jr <bgunnjr@gmail.com>
2020-08-15 01:05:15 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,61 @@
 class Owner
-  # code goes here
+  attr_reader :species, :name 
+  attr_accessor :pets
+  @@all = []
+  
+  def initialize(species)
+    @name = name
+    @species = "human"
+    @@all << self
+    @pets = {:dogs => [], :cats => []}
+  end
+
+  def buy_cat(cat)
+    @pets[:cats] << Cat.new(cat)
+  end
+
+  def buy_dog(dog)
+    @pets[:dogs] << Dog.new(dog)
+  end
+
+  def walk_dogs
+    @pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def feed_cats
+    @pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    @pets.each do |type, animals|
+      animals.each do |animal|
+        animal.mood = "nervous"
+      end
+      animals.clear
+    end
+  end
+
+  def list_pets
+    return "I have #{@pets[:dogs].size} dog(s), and #{@pets[:cats].size} cat(s)."
+  end
+
+  def say_species
+    return "I am a human."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all = []
+  end
+
+  def self.count
+    @@all.count
+  end
 end
